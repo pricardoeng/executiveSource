1598f3b59b7208d172651a25a865831cdc118ea3
PAULO RICARDO DA SILVA <paulo.silva@gupy.com.br>
2026-05-08 21:46:00 -0300

    Branding Precision: Update LinkedIn icon to pixel-perfect official square brand logo

--- script.js
+++ script.js
@@ -98,8 +98,8 @@ function getAvatarHTML(candidate) {
     return `<div class="avatar-circle">${initials}</div>`;
 }
 
-// NEW LINKEDIN ICON SVG (Square blue as requested)
-const linkedinIconSVG = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/></svg>`;
+// NEW LINKEDIN ICON SVG (Official Square Brand Logo)
+const linkedinIconSVG = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 34 34" fill="white"><path d="M34,2.5C34,1.1,32.9,0,31.5,0h-29C1.1,0,0,1.1,0,2.5v29C0,32.9,1.1,34,2.5,34h29c1.4,0,2.5-1.1,2.5-2.5V2.5z M10.2,28.7H5.1V12.1h5.1V28.7z M7.7,9.9c-1.6,0-3-1.3-3-3c0-1.6,1.3-3,3-3s3,1.3,3,3C10.6,8.6,9.3,9.9,7.7,9.9z M28.7,28.7h-5.1v-8c0-1.9,0-4.3-2.6-4.3c-2.6,0-3,2-3,4.2v8.1h-5.1V12.1h4.9v2.3h0.1c0.7-1.3,2.4-2.7,4.8-2.7c5.1,0,6.1,3.4,6.1,7.8V28.7z"/></svg>`;
 
 // Dashboard Stats & Saved Table
 function updateDashboardStats() {
